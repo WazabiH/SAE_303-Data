fd2bc15de51609bbab7cd808a686ad8ec17761fe
WazabiH <hamedwassila2004@gmail.com>
2024-12-02 18:43:27 +0100

    Update film.js

--- js/film.js
+++ js/film.js
@@ -1,4 +1,4 @@
-// Charger le fichier JSON et afficher les films
+// Charger le fichier JSON et afficher les films seulement si la barre de recherche contient un texte
 fetch('../data/movies.json') // Ajuster le chemin selon la structure du projet
     .then(response => {
         if (!response.ok) {
@@ -24,6 +24,7 @@ fetch('../data/movies.json') // Ajuster le chemin selon la structure du projet
                         <p>Bechdel Score : <span class="bechdel-score">${movie.bechdel_score}</span></p>
                         <p>Langue : ${movie.lang}</p>
                         <p>Type de Média : ${movie.media_type}</p>
+                        <p>Résumé : ${movie.overview || 'Résumé non disponible.'}</p>
                         <a href="https://www.imdb.com/title/${movie.imdbid_full}" target="_blank">Voir sur IMDb</a>
                     </div>
                 `;
@@ -31,19 +32,21 @@ fetch('../data/movies.json') // Ajuster le chemin selon la structure du projet
             });
         }
 
-        // Affiche initialement tous les films
-        displayFilms(data);
-
-        // Ajoute un événement pour filtrer les films
+        // Ajoute un événement pour filtrer les films lorsque l'utilisateur entre un texte
         searchBar.addEventListener('input', (event) => {
             const searchTerm = event.target.value.toLowerCase();
-            const filteredFilms = data.filter(movie => 
-                movie.title.toLowerCase().includes(searchTerm) || 
-                (movie.title_fr && movie.title_fr.toLowerCase().includes(searchTerm)) ||
-                movie.lang.toLowerCase().includes(searchTerm) ||
-                movie.media_type.toLowerCase().includes(searchTerm)
-            );
-            displayFilms(filteredFilms);
+            // Affiche les films uniquement si un texte est saisi
+            if (searchTerm.trim() !== '') {
+                const filteredFilms = data.filter(movie => 
+                    movie.title.toLowerCase().includes(searchTerm) || 
+                    (movie.title_fr && movie.title_fr.toLowerCase().includes(searchTerm)) ||
+                    movie.lang.toLowerCase().includes(searchTerm) ||
+                    movie.media_type.toLowerCase().includes(searchTerm)
+                );
+                displayFilms(filteredFilms);
+            } else {
+                filmList.innerHTML = ''; // Si la barre de recherche est vide, on cache les films
+            }
         });
     })
     .catch(error => console.error('Erreur:', error));
