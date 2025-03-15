05c401ab76c37ac54eb40bc1858eb890838ab5e4
WazabiH <hamedwassila2004@gmail.com>
2025-03-15 10:42:30 +0100

    h

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="container-footer">
+      <div class="logos-footer">
+        <img src="media/Logo.png" alt="Logo du site" class="logo-footer-site" />
+        <img src="media/CY-IUT_coul240.png" alt="Logo du site" class="logo-footer-cyu" />
+       </div>
+        <nav class="navigation-externe">
+          <a href="https://www.facebook.com/CYCergyParisUniversite/?locale=fr_FR" target="_blank" class="lien-reseau-sociaux">Facebook</a>
+          <a href="https://twitter.com/UniversiteCergy?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" class="lien-reseau-sociaux">Twitter</a>
+          <a href="https://www.cyu.fr/" target="_blank" class="lien-reseau-sociaux">Université CY Paris Université</a>
+          <a href="#top" class="lien-reseau-sociaux">• Remonter •</a>
+      </nav>
+      <p>© 2024-2025 - BUT MMI - CYU Paris Université. Tous droits réservés.</p>
+    </div>
+  </footer>
---
+++ header.php
@@ -0,0 +1,11 @@
+<head>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<header>
+    <h1>Mon Site</h1>
+    <nav>
+        <a href="index.php">Accueil</a>
+        <a href="about.php">À Propos</a>
+        <a href="contact.php">Contact</a>
+    </nav>
+</header>
